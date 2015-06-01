145648974d5522ce84381be2eacedb97fba56763
Kouji Watanabe <kouji.wtnb@gmail.com>
2015-06-01 23:44:31 +0900

    add controller

---
+++ src/main/java/spring_myapp/controller/SimpleController.java
@@ -0,0 +1,13 @@
+package spring_myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SimpleController {
+	@RequestMapping("/simple")
+	public @ResponseBody String simple(){
+		return "Hello World";
+	}
+}
