6d52026e940f8e3603c441174bb589985de8c295
Kouji Watanabe <kouji.wtnb@gmail.com>
2015-06-01 23:58:45 +0900

    add controller and mapping

--- src/main/java/spring_myapp/controller/SimpleController.java
+++ src/main/java/spring_myapp/controller/SimpleController.java
@@ -2,6 +2,7 @@ package spring_myapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,5 +10,10 @@ public class SimpleController {
 	@RequestMapping("/simple")
 	public @ResponseBody String simple(){
 		return "Hello World";
+	}
+	
+	@RequestMapping(value="/simple/revisited", method=RequestMethod.GET, headers="Accept=text/plain")
+	public @ResponseBody String simple2(){
+		return "Hello World Revisited!!";
 	}
 }
